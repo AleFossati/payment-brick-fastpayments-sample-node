6001e35a09bf2a1c7f354d59d1e8e4be0d1234f0
alfossati_meli <alexandre.fossati@mercadolivre.com>
2025-07-11 09:15:40 -0300

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,121 @@
+const mercadoPagoPublicKey = document.getElementById("mercado-pago-public-key").value;
+const mercadopago = new MercadoPago(mercadoPagoPublicKey);
+let paymentBrickController;
+let authenticator;
+let fastPaymentToken;
+
+async function loadPaymentForm() {
+    const productCost = parseFloat(document.getElementById('amount').value).toFixed(2);
+    const userEmail = "your_customer_email@mail.com";
+    
+    try {
+        // Step 1: Initialize authenticator
+        authenticator = await mercadopago.authenticator(productCost, userEmail);
+        
+        // Step 2: Generate fastPaymentToken
+        fastPaymentToken = await authenticator.show();
+        
+        // Step 3: Render Payment Brick
+        await renderPaymentBrick();
+        
+    } catch (error) {
+        alert("Error loading payment form, details in console");
+        console.error(error);
+    }
+}
+
+async function renderPaymentBrick() {
+    const settings = {
+        initialization: {
+            fastPaymentToken: fastPaymentToken,
+        },
+        callbacks: {
+            onReady: () => {
+                console.log('brick ready')
+            },
+            onError: (error) => {
+                alert(JSON.stringify(error))
+            },
+            onSubmit: (paymentData) => {
+                proccessPayment(paymentData)
+            }
+        },
+        locale: 'en',
+        customization: {
+            visual: {
+                style: {
+                    theme: 'dark',
+                    customVariables: {
+                        formBackgroundColor: '#1d2431',
+                        baseColor: 'aquamarine'
+                    }
+                }
+            }
+        },
+    }
+
+    const bricks = mercadopago.bricks();
+    paymentBrickController = await bricks.create('payment', 'mercadopago-bricks-contaner__PaymentCard', settings);
+}
+
+const proccessPayment = (cardFormData) => {
+    fetch("/process_payment", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cardFormData),
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        if(!result.hasOwnProperty("error_message")) {
+            document.getElementById("payment-id").innerText = result.id;
+            document.getElementById("payment-status").innerText = result.status;
+            document.getElementById("payment-detail").innerText = result.detail;
+            $('.container__payment').fadeOut(500);
+            setTimeout(() => { $('.container__result').show(500).fadeIn(); }, 500);
+        } else {
+            alert(JSON.stringify({
+                status: result.status,
+                message: result.error_message
+            }))
+        }
+    })
+    .catch(error => {
+        alert("Unexpected error\n"+JSON.stringify(error));
+    });
+}
+
+// Handle transitions
+document.getElementById('checkout-btn').addEventListener('click', function(){
+    $('.container__cart').fadeOut(500);
+    setTimeout(() => {
+        loadPaymentForm();
+        $('.container__payment').show(500).fadeIn();
+    }, 500);
+});
+
+document.getElementById('go-back').addEventListener('click', function(){
+    $('.container__payment').fadeOut(500);
+    setTimeout(() => { $('.container__cart').show(500).fadeIn(); }, 500);
+});
+
+// Handle price update
+function updatePrice(){
+    let quantity = document.getElementById('quantity').value;
+    let unitPrice = document.getElementById('unit-price').innerText;
+    let amount = parseInt(unitPrice) * parseInt(quantity);
+
+    document.getElementById('cart-total').innerText = '$ ' + amount;
+    document.getElementById('summary-price').innerText = '$ ' + unitPrice;
+    document.getElementById('summary-quantity').innerText = quantity;
+    document.getElementById('summary-total').innerText = '$ ' + amount;
+    document.getElementById('amount').value = amount;
+};
+
+
+
+document.getElementById('quantity').addEventListener('change', updatePrice);
+updatePrice();
